f33f65e1e6ec65f0a96caebcfc319b05866e7426
stonxzz <cuentamlgpro@gmail.com>
2024-08-07 17:27:41 -0600

    feature: :sparkles: add the file while.js and doWhile.js

---
+++ structures/while.js
@@ -0,0 +1,14 @@
+/*
+    while es un ciclo para poder iterar hasta que la condicion no se cumpla
+    estructura
+    while(condicion){
+        código
+    }
+*/
+
+let contador = 0;
+
+while(contador<10){
+    console.log(contador);
+    contador++;
+}
