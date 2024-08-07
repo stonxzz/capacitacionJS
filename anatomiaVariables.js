c36e37b508df18bc0348b1b3bcc4f81f0dcd7a68
stonxzz <cuentamlgpro@gmail.com>
2024-08-07 13:55:49 -0600

    feature: :sparkles: add type of data file

--- anatomiaVariables.js
+++ anatomiaVariables.js
@@ -1,2 +1,16 @@
 let cajaDeAndy = 'Woody'
 console.log(cajaDeAndy)
+
+// Lo no permitido
+let c = 'Wooody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+//Lo permitido
+let primerTrasteoDeAndy = 'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '12345'
+
+//let se usa para declarar variables las cuales su valor va a estar cambiando durante la ejecucion del program
+//const se utiliza para constantes es decir siempre seran lo mismo durante el programa
+
