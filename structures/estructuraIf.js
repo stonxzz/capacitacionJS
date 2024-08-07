6bf91aa95d2751b87d49c9dc9c9936a193106104
stonxzz <cuentamlgpro@gmail.com>
2024-08-07 15:50:53 -0600

    feature: :sparkles: add estructuraIf.js file

---
+++ structures/estructuraIf.js
@@ -0,0 +1,9 @@
+let nombre = 'Saul';
+
+if(nombre === 'Diego'){
+    console.log('Hola Diego.')
+}else if(nombre === 'Nico'){
+    console.log('Hola Nico')
+}else{
+    console.log('Tu no eres Nico ni Diego')
+};
