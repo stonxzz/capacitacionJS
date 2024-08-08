e26dd892985385511f45ba68e88fbe87a68c8c20
stonxzz <cuentamlgpro@gmail.com>
2024-08-08 17:23:51 -0600

    feature: :sparkles: I do a exercise of how use the length of an array

--- arrays/introductionArrays.js
+++ arrays/introductionArrays.js
@@ -41,3 +41,26 @@ console.log(fruits[0])
 //length property
 const numerOfFruits = fruits.length
 console.log(numerOfFruits)
+
+//Mutability
+fruits.push('watermelon');
+console.log(fruits)
+
+//Inmutability, no todos los metodos modifican el array original
+const newFruits = fruits.concat(['grape', 'kiwi'])
+console.log(fruits)
+console.log(newFruits)
+
+//checkin arrrays with Array.isArray()
+const isArray = Array.isArray(fruits)
+console.log(isArray)
+
+//Practical exercise: sum all elements of an array.
+const numbersArray = [1,2,3,4,5]
+let sum = 0;
+
+for(let i = 0; i<numbersArray.length;i++){
+    sum+= numbersArray[i];
+}
+
+console.log(sum)
